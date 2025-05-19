8af2db0ae7bfdf1219f7444483cc1572402d5dbd
RicardoSMtz <santmrich@gmail.com>
2025-05-19 08:50:04 -0600

    Pruebas Ejercicio 2.

--- app/src/main/java/com/example/ejercicio2/data/remote/model/util/Constants.kt
+++ app/src/main/java/com/example/ejercicio2/data/remote/model/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.ejercicio2.data.remote.model.util
 
 object Constants {
     const val BASE_URL = "https://dragonball-api.com/"
+    //const val BASE_URL = "https://rickandmortyapi.com/api/"
 }
